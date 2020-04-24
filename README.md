f68a228326ed52dc956fae8b0aab1a70bf82594c
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2020-04-24 15:48:53 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spilt-Milk
---
+++ SpiltMilk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpiltMilk
+//
+//  Created by Beatriz Carlos on 24/04/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
