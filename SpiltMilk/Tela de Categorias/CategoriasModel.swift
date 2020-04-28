614ba373ea5d8bfde8d51052fedbedac12f9c25e
jhennyferOliveira <jhennyfer519@gmail.com>
2020-04-28 10:46:32 -0300

    arquivos relacionados ao storyboard criados

---
+++ SpiltMilk/Tela de Categorias/CategoriasModel.swift
@@ -0,0 +1,9 @@
+//
+//  ModelCategorias.swift
+//  SpiltMilk
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 28/04/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import Foundation
