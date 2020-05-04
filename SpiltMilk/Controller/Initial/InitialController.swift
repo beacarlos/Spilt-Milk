5771113b01b93c2bb4e7aea2f12821003aa238ed
jhennyferOliveira <jhennyfer519@gmail.com>
2020-05-04 16:50:42 -0300

    criando tela inicial

---
+++ SpiltMilk/Controller/Initial/InitialController.swift
@@ -0,0 +1,81 @@
+//
+//  InitialController.swift
+//  SpiltMilk
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 04/05/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum tappedButton{
+    case signIn
+    case register
+    case guest
+}
+
+class InitialController : UIViewController{
+    
+    @IBOutlet weak var welcomeText: UILabel!
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var logoTitle: UILabel!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var viewDetail: UIView!
+    @IBOutlet weak var guestButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        viewDetail.layer.cornerRadius = 50
+        logoTitle.text = "Spilt Milk"
+        welcomeText.text = "Discuta, aprenda e compartilhe experiêcias sobre a Alergia à Proteína do Leite de Vaca"
+        styleSignInButton(button: signInButton)
+        styleRegisterButton(button: registerButton)
+        buttonAction(button: signInButton)
+        buttonAction(button: registerButton)
+        buttonAction(button: guestButton)
+    }
+    
+    func styleSignInButton(button: UIButton){
+        button.backgroundColor = #colorLiteral(red: 0.3882352941, green: 0.7019607843, blue: 0.7333333333, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 27
+    }
+    
+    func styleRegisterButton(button: UIButton){
+        button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        button.setTitleColor(#colorLiteral(red: 0.3882352941, green: 0.7019607843, blue: 0.7333333333, alpha: 1) , for: .normal)
+        button.layer.cornerRadius = 27
+        button.layer.borderColor = #colorLiteral(red: 0.3882352941, green: 0.7019607843, blue: 0.7333333333, alpha: 1)
+        button.layer.borderWidth = 2
+    }
+    
+    func buttonAction(button: UIButton){
+        button.addTarget(self, action: #selector(updateView), for: .touchUpInside)
+        
+    }
+    
+    @objc func updateView(button: UIButton) {
+        var tappedButton: tappedButton
+        let buttonTitle = button.currentTitle
+        
+        if buttonTitle == "Register" {
+            tappedButton = .register
+        } else if buttonTitle == "Sign In"{
+            tappedButton = .signIn
+        }else{
+            tappedButton = .guest
+        }
+        switch tappedButton {
+        case .signIn:
+            print("go to log in screen")
+        case .register:
+            print("go to create account screen")
+        case .guest:
+            print("go to categories screen")
+        }
+    }
+}
+
+
