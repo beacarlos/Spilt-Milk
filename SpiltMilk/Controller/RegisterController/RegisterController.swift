263ba2319b4bcf53c80e96ddec3e75126afeb7d3
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2020-05-05 16:37:29 -0300

    Tela finalizada

--- SpiltMilk/Controller/RegisterController/RegisterController.swift
+++ SpiltMilk/Controller/RegisterController/RegisterController.swift
@@ -9,41 +9,33 @@
 import Foundation
 import UIKit
 
-class RegisterController: UIViewController {
+class RegisterController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet var textFields: [UITextField]!
-    
     @IBOutlet weak var nomeTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var senhaTextField: UITextField!
     @IBOutlet weak var cSenhaTextField: UITextField!
     @IBOutlet weak var checkBox: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // User image view
+        // User image view.
         userImageView.image = #imageLiteral(resourceName: "RegisterLogo")
+        userImageView.layer.cornerRadius = signUpButton.frame.height / 2
         
+        // Deixar a sign button redonda.
         signUpButton.layer.cornerRadius = signUpButton.frame.height / 3
         
-//        configureTextField()
-        configtextField(textField: nomeTextField)
-        configtextField(textField: emailTextField)
-        configtextField(textField: senhaTextField)
-        configtextField(textField: cSenhaTextField)
-
-    }
-    
-    func configureTextField() {
-        let bottomLine = CALayer()
-        for textField in textFields {
-            bottomLine.frame = CGRect(x: 0, y: textField.frame.height - 2, width: textField.frame.width, height: 2)
-            bottomLine.backgroundColor = UIColor(red: 0.39, green: 0.70, blue: 0.73, alpha: 1.00).cgColor
-            textField.borderStyle = .none
-            textField.layer.addSublayer(bottomLine)
-        }
+        textFields.forEach { configtextField(textField: $0) }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
+        self.hideKeyboardWhenTappedAround()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(self.dismissKeyboard)))
     }
     
     func configtextField(textField: UITextField) {
@@ -53,7 +45,7 @@ class RegisterController: UIViewController {
         textField.borderStyle = .none
         textField.layer.addSublayer(bottomLine)
     }
-
+    
     @IBAction func checkBoxAction(_ sender: UIButton) {
         let uncheck = #imageLiteral(resourceName: "uncheck")
         let check = #imageLiteral(resourceName: "check")
@@ -65,19 +57,69 @@ class RegisterController: UIViewController {
             checkBox.setImage(uncheck, for: .normal)
             sender.isSelected = true
         }
-        
     }
     
-//    @IBAction func checkBoxAction(_ sender: UIButton) {
-//        let image = UIImage(named: "checkmark.square.fill")
-//        if sender.isSelected == true {
-//            checkBox.setImage(image, forState: .normal)
-//            sender.isSelected = false
-//        } else {
-//            checkBox.setImage(UIImage(named: "square"), for: .normal)
-//            sender.isSelected = true
-//        }
-//        
-//    }
+    // DataPiker
+    @IBAction func chooseImageAction(_ sender: UIButton) {
+        // controlador de seleção de imagens.
+        let imagePickerController = UIImagePickerController()
+        
+        
+        let actionSheet = UIAlertController(title: "Selecione uma foto", message: "Escolhe uma foto.", preferredStyle: .actionSheet)
+        imagePickerController.delegate = self
+        
+        // Opção da câmera
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action: UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                print("Camera not available.")
+            }
+        }))
+        
+        // Opção da galeria
+        actionSheet.addAction(UIAlertAction(title: "Galeira de fotos", style: .default, handler: { (action: UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey(rawValue: UIImagePickerController.InfoKey.originalImage.rawValue)] as! UIImage
+        userImageView.image = image
+        userImageView.layer.cornerRadius = signUpButton.frame.height / 2
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func keyboardWillShow(sender: NSNotification){
+        self.view.frame.origin.y = -150
+    }
+    
+    @objc func keyboardWillHide(sender: NSNotification){
+        self.view.frame.origin.y = 0
+    }
+    
+    func hideKeyboardWhenTappedAround(){
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard(){
+        self.view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
