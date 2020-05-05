f71b4254bc4a962e6c8affb264bf2746d68f2ba5
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2020-05-04 22:33:17 -0300

    Tela de resgistro quase finalizada. Falta: configurações de keyboard.

--- SpiltMilk/Controller/RegisterController/RegisterController.swift
+++ SpiltMilk/Controller/RegisterController/RegisterController.swift
@@ -6,15 +6,78 @@
 //  Copyright © 2020 Beatriz Carlos. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class RegisterController: UIViewController {
+    
     @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet var textFields: [UITextField]!
+    
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var senhaTextField: UITextField!
+    @IBOutlet weak var cSenhaTextField: UITextField!
+    @IBOutlet weak var checkBox: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        userImageView.image = #imageLiteral(resourceName: "joao")
-            userImageView.layer.cornerRadius = userImageView.frame.size.width / 2
-//        userImageView.bounds = .null
+        // User image view
+        userImageView.image = #imageLiteral(resourceName: "RegisterLogo")
+        
+        signUpButton.layer.cornerRadius = signUpButton.frame.height / 3
+        
+//        configureTextField()
+        configtextField(textField: nomeTextField)
+        configtextField(textField: emailTextField)
+        configtextField(textField: senhaTextField)
+        configtextField(textField: cSenhaTextField)
+
     }
+    
+    func configureTextField() {
+        let bottomLine = CALayer()
+        for textField in textFields {
+            bottomLine.frame = CGRect(x: 0, y: textField.frame.height - 2, width: textField.frame.width, height: 2)
+            bottomLine.backgroundColor = UIColor(red: 0.39, green: 0.70, blue: 0.73, alpha: 1.00).cgColor
+            textField.borderStyle = .none
+            textField.layer.addSublayer(bottomLine)
+        }
+    }
+    
+    func configtextField(textField: UITextField) {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: textField.frame.height - 2, width: textField.frame.width, height: 2)
+        bottomLine.backgroundColor = UIColor(red: 0.39, green: 0.70, blue: 0.73, alpha: 1.00).cgColor
+        textField.borderStyle = .none
+        textField.layer.addSublayer(bottomLine)
+    }
+
+    @IBAction func checkBoxAction(_ sender: UIButton) {
+        let uncheck = #imageLiteral(resourceName: "uncheck")
+        let check = #imageLiteral(resourceName: "check")
+        
+        if sender.isSelected == true {
+            checkBox.setImage(check, for: .normal)
+            sender.isSelected = false
+        } else {
+            checkBox.setImage(uncheck, for: .normal)
+            sender.isSelected = true
+        }
+        
+    }
+    
+//    @IBAction func checkBoxAction(_ sender: UIButton) {
+//        let image = UIImage(named: "checkmark.square.fill")
+//        if sender.isSelected == true {
+//            checkBox.setImage(image, forState: .normal)
+//            sender.isSelected = false
+//        } else {
+//            checkBox.setImage(UIImage(named: "square"), for: .normal)
+//            sender.isSelected = true
+//        }
+//        
+//    }
 }
+
