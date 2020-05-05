fd13f46112d65b184146f77ace4b8dd7f57906e7
jl <jl>
2020-05-05 10:06:54 -0300

    adicionando tela de recuperar senha

---
+++ SpiltMilk/Controller/RecoverController/RecoverPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecoverPasswordViewController.swift
+//  SpiltMilk
+//
+//  Created by Jessica Guiot on 05/05/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import UIKit
+
+class RecoverPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
